5246b6a58c8dcaac4cf2bc8ce11b67aeb318789c
vinishasinha <159924188+vinishasinha@users.noreply.github.com>
2024-06-21 14:26:58 +0530

    Charts.js

--- Charts.js
+++ Charts.js
@@ -1,11 +1,10 @@
-// src/pages/Charts.js
+
 import React from 'react';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 const data = [
   { name: 'Jan', uv: 400, pv: 2400, amt: 2400 },
   { name: 'Feb', uv: 300, pv: 1398, amt: 2210 },
-  // Add more data points as needed
 ];
 
 const Charts = () => {
